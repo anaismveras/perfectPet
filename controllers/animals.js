81258c98fe3e8b6860df48f4d9d96d9f0d079381
anaismveras <verasmanais@gmail.com>
2021-11-15 13:56:34 -0500

    trying to figure out how to get the token to work for an idea call

--- controllers/animals.js
+++ controllers/animals.js
@@ -2,16 +2,37 @@ const { default: axios } = require('axios');
 const express = require('express');
 const router = express.Router();
 
+const petFinderKey = process.env.PET_FINDER_API_KEY
+const petFinderSecret = process.env.PET_FINDER_SECRET
+
+function getData () {
+    const token = ''
+    axios.post('https://api.petfinder.com/v2/animals', {
+
+    }, {
+        headers: {
+
+        }
+    })
+    // const tokenUrl = `'grant_type=client_credentials&client_id=${petFinderKey}&client_secret=${petFinderSecret}' https://api.petfinder.com/v2/oauth2/token`
+    // axios.get(tokenUrl.access_token)
+    // .then(apiToken => {
+    //     let petFinderUrl = `'Authorization: Bearer ${apiToken}' https://api.petfinder.com/v2/animals`
+    //     console.log(petFinderUrl)
+    // })
+    // console.log(accessToken)
+    // axios.get(petFinderUrl)
+    // .then(apiRes => {
+    //     let animals = apiRes.data.animals
+    //     res.send(`this is the type of animals, ${animals}`)
+    // })
+    // .catch(error => {
+    //     console.log(error)
+    // })
+}
+
 router.get('/', (req, res) => {
-    let petFinderUrl = `https://api.petfinder.com/v2/animals`
-    axios.get(petFinderUrl)
-    .then(apiRes => {
-        let animals = apiRes.animals
-        res.send(`this is the type of animals, ${animals}`)
-    })
-    .catch(error => {
-        console.log(error)
-    })
+    getData()
 })
 
 module.exports = router
